6f3190d706ebeb28af3d697e0b41176dabd686bf
H41zy <yziahguerrero@gmail.com>
2026-06-12 12:55:11 +0800

    Add files via upload

---
+++ src/main/java/com/example/findit/dao/ActivityLogDAO.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.sql.*;
+
+public class ActivityLogDAO {
+
+    public void log(
+            int userId,
+            String action,
+            String description){
+
+        String sql =
+                """
+                INSERT INTO activity_logs
+                (user_id, action, description)
+                VALUES (?, ?, ?)
+                """;
+
+        try(Connection conn =
+                    DBConnection.connect();
+
+            PreparedStatement ps =
+                    conn.prepareStatement(sql)){
+
+            ps.setInt(1, userId);
+            ps.setString(2, action);
+            ps.setString(3, description);
+
+            ps.executeUpdate();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
